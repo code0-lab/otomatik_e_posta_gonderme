b7c8787e4dfdbfd326ab6302d2db7199047723bb
code0-lab <57825302+code0-lab@users.noreply.github.com>
2023-08-03 21:40:48 +0300

    Update e_posta.main.py

--- e_posta.main.py
+++ e_posta.main.py
@@ -3,8 +3,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 def send_email(sender_email, sender_password, receiver_email, subject, message):
-    # E-posta sunucusuna bağlanma
-    smtp_server = "smtp.gmail.com"  # Eğer başka bir sağlayıcı kullanıyorsanız, sunucu adresini değiştirin.
+    smtp_server = "smtp.gmail.com"  
     smtp_port = 587
     try:
         server = smtplib.SMTP(smtp_server, smtp_port)
@@ -16,21 +15,21 @@ def send_email(sender_email, sender_password, receiver_email, subject, message):
 
 
 
-    # E-posta oluşturma
+    
     msg = MIMEMultipart()
     msg["From"] = sender_email
     msg["To"] = receiver_email
     msg["Subject"] = subject
     msg.attach(MIMEText(message, "plain"))
 
-    # E-postayı gönderme
+
     try:
         server.sendmail(sender_email, receiver_email, msg.as_string())
         print("E-posta gönderildi.")
     except Exception as e:
         print("E-posta gönderme hatası:", str(e))
 
-    # E-posta sunucusu bağlantısını kapatma
+
     server.quit()
 if __name__ == "__main__":
     sender_email = input("e_mailiniz: ")
